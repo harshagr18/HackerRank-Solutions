1498ae890ea970e8633da132d0a8ec3899512bb0
harshagr18 <harsh.agrawal17@siesgst.ac.in>
2020-08-26 11:22:17 +0530

    Updating sample

--- sample.py
+++ sample.py
@@ -1,7 +1,7 @@
-#Problem Link : 
+# Problem Link : 
 
 u = int(input())
-for o in range(u):
+for _ in range(u):
     a = int(input())
     a,b = list(map(int, input().split(" ")))
     a = list(map(int,input().split()))
