fb66870b41acaac79b79c87814bd02c1ac9d76dc
dsikligar <sikligardhaval@gmail.com>
2021-10-05 00:10:50 -0400

    Commit message

--- designer-door-mat.py
+++ designer-door-mat.py
@@ -1,8 +1,25 @@
-# Problem Link : https://www.hackerrank.com/challenges/designer-door-mat/problem
+# Python program for implementation of Bubble Sort
 
-n, m = map(int, input().split())
-for i in range(1, n, 2):
-    print(('.|.'*i).center(m, '-'))
-print('WELCOME'.center(m, '-'))
-for i in range(n-2, -1, -2):
-    print(('.|.'*i).center(m, '-'))
+def bubbleSort(arr):
+	n = len(arr)
+
+	# Traverse through all array elements
+	for i in range(n):
+
+		# Last i elements are already in place
+		for j in range(0, n-i-1):
+
+			# traverse the array from 0 to n-i-1
+			# Swap if the element found is greater
+			# than the next element
+			if arr[j] > arr[j+1] :
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+
+# Driver code to test above
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+bubbleSort(arr)
+
+print ("Sorted array is:")
+for i in range(len(arr)):
+	print ("%d" %arr[i]),
